f0d75e5456b1f7297e82671656e36a99efbbcf6b
SeonJungKim <ksj083895@gmail.com>
2021-03-07 10:29:35 +0500

    Initial Commit

---
+++ SwiftUI_Redacted_TUT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Redacted_TUT
+//
+//  Created by 김선중 on 2021/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Redacted_TUT/SwiftUI_Redacted_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Redacted_TUTApp.swift
+//  SwiftUI_Redacted_TUT
+//
+//  Created by 김선중 on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Redacted_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
